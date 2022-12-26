428fd04a4056962c513afde84cf37b59a4561476
ryanlee13288 <leejy-wm19@student.tarc.edu.my>
2022-12-26 12:43:31 +0800

    Create download_package.py

---
+++ download_package.py
@@ -0,0 +1,7 @@
+sudo pip install pyqt6==6.4.0
+sudo pip install smtplib
+sudo pip install ssl
+sudo pip install psutil
+sudo apt install python3-scapy -y
+sudo pip install tk
+sudo python -m pip install scipy
